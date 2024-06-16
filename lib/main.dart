b2b2fe07b6b56adae4a9987b7a6ee2284f36bbd9
Yohwan Lim <dyghks7102@naver.com>
2024-06-16 23:15:05 +0900

    feat: 지갑 UI 따라잡기1

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,104 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
+
 class App extends StatelessWidget {
+  const App({super.key});
 
   @override
-  Widget build(BuildContext context) { // Flutter에서 build 메서드를 통해 위젯을 만듬
-    // root App Widget은 MaterialApp(구글) 혹은 CupertinoApp(애플)을 리턴해야함
-    // Flutter는 구글에서 개발하였기에 MaterialApp이 나음
-    // 모바일 앱은 Scaffold 구조를 가져야함
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello flutter!'),
-        ),
-        body: Center(
-          child: Text('Hello world!'),
+        backgroundColor: const Color(0xFF181818), // 특정 컬러를 사용하기 위한 방법
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 40,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment
+                    .end, // Row의 MainAxis = 수평 가로 방향, CrossAxis = 수직 세로 방향
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment
+                        .end, // Column의 MainAxis = 수직 세로 방향, CrossAxis = 수평 가로 방향
+                    children: [
+                      const Text(
+                        'Hey, Selena',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.8),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 120,
+              ),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              const Text(
+                '\$5 194 482',
+                style: TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 50,
+                      ),
+                      child: Text(
+                        'Transfer',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
-    // return CupertinoApp();
   }
 }
