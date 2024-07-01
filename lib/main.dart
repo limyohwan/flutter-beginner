7f961cee77f0c02e2ff29db7c044184e45f9ccfa
Yohwan Lim <dyghks7102@naver.com>
2024-07-01 23:38:15 +0900

    feat: 웹툰 앱바 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/study/pomodoro.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
-  runApp(const Pomodoro());
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key}); // widget은 ID같은 식별자 역할을 하는 key가 존재함
+  //Flutter가 위젯을 빠르게 찾기(식별하기)위해 존재함
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
 }
