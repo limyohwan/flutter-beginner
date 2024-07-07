9709df513535363ac1d20df36bf557e25b259525
Yohwan Lim <dyghks7102@naver.com>
2024-07-07 23:16:49 +0900

    feat: Url Laucher를 활용하여 browser 연동하여 웹툰 보여주기

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,8 @@ void main() {
 
 class App extends StatelessWidget {
   const App({super.key}); // widget은 ID같은 식별자 역할을 하는 key가 존재함
-  //Flutter가 위젯을 빠르게 찾기(식별하기)위해 존재함
+  // Flutter가 위젯을 빠르게 찾기(식별하기)위해 존재함
+  // ios나 android 파일을 변경하면 hot reloading이 안되므로 다시 빌드하여 시작해야함
 
   @override
   Widget build(BuildContext context) {
